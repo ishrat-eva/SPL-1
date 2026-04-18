4e097306d4c8eb1bc45a3606d0f4af96238e9f01
Ishrat Eva <ishrateva31@gmail.com>
2026-04-18 10:53:51 +0600

    updated covolutionalLayer

--- src/convolutionalLayer.h
+++ src/convolutionalLayer.h
@@ -17,61 +17,101 @@ public:
     int outputHeight;    
     
     vector<Matrix> filters;  
-    vector<double> biases;   
-    
-  
-    ConvolutionalLayer(int numF, int fSize, int inW, int inH) {
-        numFilters = numF;
-        filterSize = fSize;
-        inputWidth = inW;
-        inputHeight = inH;
+    vector<double> biases;  
+
+  Matrix lastInput;
+    vector<Matrix> lastPreActivation; 
+
+           ConvolutionalLayer(int numF, int fSize, int inW, int inH)
+        : numFilters(numF), filterSize(fSize),
+          inputWidth(inW), inputHeight(inH),
+          lastInput(inH, inW)
+    {
         
    
         outputWidth = inputWidth - filterSize + 1;
         outputHeight = inputHeight - filterSize + 1;
-        
        
+ 
+
+
         for(int i = 0; i < numFilters; i++) {
-            Matrix filter(filterSize, filterSize);
-            filter.randomize(-0.1, 0.1);  
-            filters.push_back(filter);
-            biases.push_back(0.0);        
+            Matrix f(filterSize, filterSize);
+            
+            double scale = sqrt(2.0 / (filterSize * filterSize));
+            for(int r = 0; r < filterSize; r++)
+                for(int c = 0; c < filterSize; c++)
+                    f.data[r][c] = ((double)rand()/RAND_MAX * 2 - 1) * scale;
+            filters.push_back(f);
+            biases.push_back(0.0);
         }
-        
-        cout << "Convolutional Layer: " << inputWidth << "x" << inputHeight 
-                  << "->" << outputWidth << "x" << outputHeight 
-                  << " (" << numFilters << " filters)" <<endl;
+
+        cout << "Convolutional Layer: " << inputWidth << "x" << inputHeight
+             << "->" << outputWidth << "x" << outputHeight
+             << " (" << numFilters << " filters)" << endl;
     }
-    
-   
+
     vector<Matrix> forward(Matrix input) {
+        lastInput = input;
+        lastPreActivation.clear();
         vector<Matrix> outputs;
-        
-     
+
         for(int f = 0; f < numFilters; f++) {
-            Matrix featureMap(outputHeight, outputWidth);
-            
+            Matrix preAct(outputHeight, outputWidth);
             for(int i = 0; i < outputHeight; i++) {
-                for(int j = 0; j < outputWidth; j++) {
-                  
-                    double sum = 0.0;
-                    
-                for(int fi = 0; fi < filterSize; fi++) {
-                    for(int fj = 0; fj < filterSize; fj++) {
-                            sum += input.data[i + fi][j + fj] * filters[f].data[fi][fj];
-                        }
-                    }
-                    
-                
-                    featureMap.data[i][j] = relu(sum + biases[f]);
-                }
-            }
-            
-            outputs.push_back(featureMap);
+            for(int j = 0; j < outputWidth; j++) {
+             double sum = biases[f];
+                for(int fi = 0; fi < filterSize; fi++)
+                    for(int fj = 0; fj < filterSize; fj++)
+                     sum += input.data[i+fi][j+fj] * filters[f].data[fi][fj];
+                    preAct.data[i][j] = sum;
+             }
+         }
+     lastPreActivation.push_back(preAct);
+     Matrix activated(outputHeight, outputWidth);
+         for(int i = 0; i < outputHeight; i++)
+            for(int j = 0; j < outputWidth; j++)
+               activated.data[i][j] = relu(preAct.data[i][j]);
+            outputs.push_back(activated);
         }
-        
         return outputs;
     }
+
+    void backward(vector<Matrix> dOut, double learningRate) {
+        for(int f = 0; f < numFilters; f++) {
+            Matrix delta(outputHeight, outputWidth);
+            for(int i = 0; i < outputHeight; i++)
+                for(int j = 0; j < outputWidth; j++)
+                    delta.data[i][j] = dOut[f].data[i][j]
+                        * reluDerivative(lastPreActivation[f].data[i][j]);
+
+            for(int fi = 0; fi < filterSize; fi++) {
+                for(int fj = 0; fj < filterSize; fj++) {
+                    double grad = 0.0;
+                    for(int i = 0; i < outputHeight; i++)
+                    for(int j = 0; j < outputWidth; j++)
+                     grad += delta.data[i][j] * lastInput.data[i+fi][j+fj];
+                    filters[f].data[fi][fj] -= learningRate * grad;
+                }
+            }
+
+     double biasGrad = 0.0;
+         for(int i = 0; i < outputHeight; i++)
+            for(int j = 0; j < outputWidth; j++)
+              biasGrad += delta.data[i][j];
+            biases[f] -= learningRate * biasGrad;
+        }
+    }
 };
+ 
 
 #endif
+
+
+
+
+  
+
+ 
+
+
